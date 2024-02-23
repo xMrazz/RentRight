42d91855c6da741642f504adf8a54c5bd99405f9
Igor <123474463+xMrazz@users.noreply.github.com>
2024-02-22 23:33:27 -0500

    Delete RentRight/backend directory

--- RentRight/backend/models/manager.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const managerSchema = new mongoose.Schema({
-    username : { type: String, unique: true, required: true, maxlength: 100 },
-    email : { type: String, unique: true, required: true, maxlength: 50 },
-    password : { type: String, required: true, maxlength: 50 },
-});
-
-module.exports = mongoose.model('Manager', managerSchema);
